bfd1d8e3f1613e714c69c9acc0292932c86ed875
Lê Minh Hoàng <hoang.le.k3set@eiu.edu.vn>
2021-08-26 09:54:49 +0700

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using books.Models.Dto;
+using books.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksService _booksService;
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allbooks = _booksService.GetAllBooks();
+            return Ok(allbooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookDto book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody]BookDto book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _booksService.DeleteBookById(id);
+            return Ok();
+        }
+    }
+}
---
+++ Models/AppDbInitializer.cs
@@ -0,0 +1,50 @@
+﻿using books.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace books.Models
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(
+                        new Book()
+                        {
+                            Title = "A Beautiful Mind",
+                            Description = "Nasar thoroughly explores Nash’s prestigious career, from his beginnings at MIT to his work at the RAND Corporation",
+                            IsRead = true,
+                            DateRead = DateTime.Now.AddDays(-10),
+                            Rate = 4,
+                            Genre = "Biography",
+                            Author = "Sylvia Nasar",
+                            CoverUrl = "https://m.media-amazon.com/images/I/511wQDs0J7L.jpg",
+                            DateAdded = DateTime.Now
+                        },
+                        new Book()
+                        {
+                            Title = "Quiet: The Power of Introverts in a World That Can't Stop Talking",
+                            Description = "Nasar thoroughly explores Nash’s prestigious career, from his beginnings at MIT to his work at the RAND Corporation",
+                            IsRead = false,
+                            Genre = "Behavioral Psychology",
+                            Author = "Susan Cain",
+                            CoverUrl = "https://m.media-amazon.com/images/P/0307352153.01._SCLZZZZZZZ_SX500_.jpg",
+                            DateAdded = DateTime.Now
+                        });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
